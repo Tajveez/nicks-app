d07315ea769fb37a8225a3a2316bb4e781dd38a7
tajveez <tajveez786@gmail.com>
2021-11-03 23:18:21 +0500

    Calls done

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Models\Post;
+use App\Models\Subscription;
+use App\Models\User;
+use App\Models\Website;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +20,58 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/posts', function () {
+    return Post::all();
+});
+
+Route::post('/posts', function () {
+    request()->validate([
+        'title' => 'required',
+        'description' => 'required'
+    ]);
+
+    return Post::create([
+        'title' => request('title'),
+        'description' => request('description')
+    ]);
+});
+
+Route::post('/subscribe', function () {
+    request()->validate([
+        'email' => 'required',
+        'website_id' => 'required'
+    ]);
+    $email = request('email');
+    $websiteId = request('website_id');
+
+    $userData = User::where('email', $email);
+    $websiteData = Website::get($websiteId);
+
+    if (!$userData) {
+        return [
+            'success' => false,
+            'message' => "User with {$email} doesn't exist"
+        ];
+    }
+
+    if (!$websiteData) {
+        return [
+            'success' => false,
+            'message' => "Website doesn't doesn't exist"
+        ];
+    }
+
+    $userId = $userData->id;
+    $success = Subscription::create([
+        'user_id' => $userId,
+        'website_id' => $websiteId
+    ]);
+
+    $message = ($success) ? "Subscribed successfully" : "Subscription faild";
+    return [
+        'success' => $success,
+        'message' => $message
+    ];
 });
