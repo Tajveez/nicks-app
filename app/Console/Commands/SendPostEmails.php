abc72db9dd4d9c6d641b951200f82623f813bab5
tajveez <tajveez786@gmail.com>
2021-11-04 00:29:55 +0500

    Mail setup

--- app/Console/Commands/SendPostEmails.php
+++ app/Console/Commands/SendPostEmails.php
@@ -3,6 +3,9 @@
 namespace App\Console\Commands;
 
 use App\Models\Post;
+use App\Mail\SubMail;
+use App\Models\Subscription;
+use App\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
@@ -41,6 +44,34 @@ class SendPostEmails extends Command
     {
         // get posts that aren't sent yet
         $posts = DB::table('posts')->where('is_sent', false)->get();
+        foreach ($posts as $post) {
+            $websiteId = $posts->website_id;
+            $subscribers = $this->getAllSubscribers($websiteId);
+
+            $this->sendEmail($subscribers, [
+                'post_title' => $post->title,
+                'post_description' => $post->description,
+            ]);
+
+            $post->update(['is_sent' => true]);
+        }
+
         return Command::SUCCESS;
+    }
+
+    public function getAllSubscribers($websiteId)
+    {
+        $websiteSubscribers = DB::table('subscriptions')->where('website_id', $websiteId)->get();
+        return $websiteSubscribers;
+    }
+
+    public function sendEmail($subscribers, $content)
+    {
+        foreach ($subscribers as $sub) {
+            $userData = User::get($sub->user_id);
+            $email = $userData->email;
+
+            Mail::to($email)->queue(new SubMail($content));
+        }
     }
 }
